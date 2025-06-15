bf2659d8a8256fcdf87e53341e1867100365651a
Bruce ZhongZp <xzhong86@163.com>
2025-06-15 23:45:06 +0800

    add f1-f12 map

--- src/json/brcz-spacefn.json.js
+++ src/json/brcz-spacefn.json.js
@@ -16,7 +16,7 @@ function genSetVar(vname, val) {
 }
 function genSpaceFnModeSwitch() {
   return {
-    description: "SpaceFN(BruceZh): Space enables SpaceFN mode " +
+    description: "SpaceFN: Mode switch " +
       "(inspired by: https://geekhack.org/index.php?topic=51069.0)",
     manipulators: [
       {
@@ -34,7 +34,10 @@ function genSpaceFnModeSwitch() {
 }
 
 function genSpaceFnMap(from_keys, to_keys) {
-  const desc = "SpaceFN: map - to -"
+  const str_f = from_keys.join("/")
+  const str_t = to_keys.join("/")
+  //const desc  = `SpaceFN: map ${str_f} to ${str_t}`
+  const desc  = ["SpaceFN: map ", str_f, " to ", str_t].join("")
   const genMap = function(from_key, to_key) {
     return {
       conditions: [
@@ -58,16 +61,41 @@ function genSpaceFnMap(from_keys, to_keys) {
   }
 }
 
+function makeArray(len, content) {
+  var result = [];
+  if(typeof content == "function") {
+    for(var i = 0; i < len; i++) {
+      result.push(content(i));
+    }
+  } else {
+    for(var i = 0; i < len; i++) {
+      result.push(content);
+    }
+  }
+  return result;
+}
+
+function numSeq(start, len, pfx) {
+  //const seq = Array(len).fill().map((_,i) => start + i)
+  //return seq.map((e, _) => pfx + e)
+  //const seq = Array(len).fill().map(function(_,i){ return start + i })
+  const seq = makeArray(len, function(i){ return start + i })
+  return seq.map(function(e, _){ return pfx + e })
+}
+
 function main() {
   console.log(
     JSON.stringify(
       {
-        title: 'input_source_if,input_source_unless example',
+        title: 'SpaceFn(BruceZh): use spacebar as Fn, by BruceZh.',
+	author: "Bruce Zhong (https://github.com/xzhong86)",
         rules: [].concat(
 	  genSpaceFnModeSwitch(),
-	  genSpaceFnMap(["u", "k", "j", "l"], ["up_arrow", "down_arrow", "left_arrow", "right_arrow"]),
+	  genSpaceFnMap(["i", "k", "j", "l"], ["up_arrow", "down_arrow", "left_arrow", "right_arrow"]),
 	  genSpaceFnMap(["h", "o", "u", "n"], ["home", "end", "page_up", "page_down"]),
-	  genSpaceFnMap(["x", "b"], ["delete_forward", "spacebar"])
+	  genSpaceFnMap(["x", "b"], ["delete_forward", "spacebar"]),
+	  genSpaceFnMap(numSeq(1, 9, "").concat("0"), numSeq(1, 10, "f")),
+	  genSpaceFnMap(["hyphen", "equal_sign"], ["f11", "f12"])
 	)
       },
       null,
